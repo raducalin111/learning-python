0c8aeb9f334e04e66084a19a8423aa5004cc4210
Calin <raducalin111@gmail.com>
2021-05-24 01:32:25 +0200

    ml

---
+++ ml/gradient_descent/gradient_descent.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def read_training_set():
+    data = pd.read_csv('data.csv')
+    return data.iloc[:, 0], data.iloc[:, 1]
+
+
+X, y = read_training_set()
+
+m = len(X)
+iterations = 1000
+alpha = 0.0001
+
+theta1 = 0
+theta2 = 0
+
+for i in range(iterations):
+    predicted_value = theta1 + X * theta2
+
+    tempTheta1 = theta1 - alpha * (1 / m) * sum(predicted_value - y)
+    tempTheta2 = theta2 - alpha * (1 / m) * sum((predicted_value - y) * X)
+
+    theta1 = tempTheta1
+    theta2 = tempTheta2
+
+predicted_value = theta1 + X * theta2
+
+plt.scatter(X, y)
+plt.plot(X, predicted_value, color='red')
+plt.show()
